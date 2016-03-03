8ab9ff75ae19aa3db8c85fc5ae72ced300127a7f
Jason Campbell <jasoncampbell@google.com>
2016-03-03 10:53:38 -0800

    reader/flutter: adds initial scaffold
    Change-Id: I02b338f9e69b697d8a086b5b07de8f2c10e0060f

---
+++ flutter/lib/main.dart
@@ -0,0 +1,39 @@
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+      title: 'Flutter Demo',
+      routes: <String, RouteBuilder>{
+        '/': (RouteArguments args) => new FlutterDemo()
+      }));
+}
+
+class FlutterDemo extends StatefulComponent {
+  _FlutterDemoState createState() => new _FlutterDemoState();
+}
+
+class _FlutterDemoState extends State<FlutterDemo> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        toolBar: new ToolBar(center: new Text('Flutter Demo')),
+        body: new Center(
+            child: new Text(
+                'Button tapped $_counter time${ _counter == 1 ? '' : 's' }.')),
+        floatingActionButton: new FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: new Icon(icon: 'content/add')));
+  }
+}
